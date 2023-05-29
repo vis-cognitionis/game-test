ecf785dd85e6726083e4cb1f57ccfdca5a5ac289
Vis Cognitionis <omeralemdar94@gmail.com>
2023-05-30 01:58:44 +0300

    Update filter_sort_modal.tsx

--- src/view/components/filter-sort/filter_sort_modal.tsx
+++ src/view/components/filter-sort/filter_sort_modal.tsx
@@ -6,7 +6,10 @@ import {
   TouchableWithoutFeedback,
   Animated,
   Text,
+  ScrollView,
+  TouchableOpacity,
 } from "react-native";
+import Chip from "./chip";
 
 const styles = StyleSheet.create({
   modalContainer: {
@@ -16,11 +19,22 @@ const styles = StyleSheet.create({
   },
 
   contentContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     backgroundColor: "white",
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
-    padding: 20,
+    padding: 28,
     height: 380,
+  },
+
+  chipContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 16,
   },
 
   title: {
@@ -29,6 +43,37 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18,
     color: "#FC4C02",
+  },
+
+  subheading: {
+    width: 104,
+    height: 25,
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#000000",
+  },
+
+  line: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "rgba(156, 152, 152, 0.47);",
+    margin: 14,
+  },
+
+  rowItemContainer: {
+    display: "flex",
+    flexDirection: "column",
+    rowGap: 8,
+    height: 70,
+    justifyContent: "center",
+  },
+
+  rowItemsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignSelf: "flex-start",
+    rowGap: 24,
+    width: "100%",
   },
 });
 
@@ -60,6 +105,25 @@ const FilterSortModal = ({ visible, onClose }: FilterSortModalProps) => {
     }
   }, [visible, translateY]);
 
+  const RowItem = ({
+    title,
+    children,
+  }: {
+    title: string;
+    children: React.ReactNode;
+  }) => {
+    return (
+      <View style={styles.rowItemContainer}>
+        <Text style={styles.subheading}>{title}</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity activeOpacity={1} style={styles.chipContainer}>
+            {children}
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    );
+  };
+
   return (
     <Modal visible={visible} transparent animationType="none">
       <TouchableWithoutFeedback onPress={handleModalClose}>
@@ -69,6 +133,24 @@ const FilterSortModal = ({ visible, onClose }: FilterSortModalProps) => {
               style={[styles.contentContainer, { transform: [{ translateY }] }]}
             >
               <Text style={styles.title}>Filter & Sort</Text>
+              <View style={styles.line} />
+              <View style={styles.rowItemsContainer}>
+                <RowItem
+                  children={
+                    <>
+                      <Chip />
+                      <Chip />
+                      <Chip />
+                      <Chip />
+                      <Chip />
+                      <Chip />
+                    </>
+                  }
+                  title="Categories"
+                />
+                <RowItem children={<Chip />} title="Platforms" />
+                <RowItem children={<Chip />} title="Sort" />
+              </View>
             </Animated.View>
           </TouchableWithoutFeedback>
         </View>
